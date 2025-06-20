42424ea08496eadd76a605f64c705ffbb9d0f8a2
AP3HIM <thefartz19@gmail.com>
2025-06-20 16:41:08 -0400

    Update vite.config.js for refresh thing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import { resolve } from 'path'
 
-
-// https://vite.dev/config/
+// https://vite.dev/config/a
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  publicDir: 'public', // this should already be default
 })
