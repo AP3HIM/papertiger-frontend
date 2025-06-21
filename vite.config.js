a54977d1ba5f4fa7a486b767bd823ccf7381f749
AP3HIM <thefartz19@gmail.com>
2025-06-20 20:49:14 -0400

    Update vite.config.js more refresh stuff

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr"
 import { resolve } from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.'  // puts it in dist/
-        }
-      ]
-    })
   ],
   build: {
     rollupOptions: {
