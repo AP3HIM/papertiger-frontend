77d0ebfe578b0b6dd6e1f03506fb5a116791e25e
AP3HIM <thefartz19@gmail.com>
2025-07-03 01:33:33 -0400

    adding seo.jsx

---
+++ src/components/SEO.jsx
@@ -0,0 +1,38 @@
+import { Helmet } from "react-helmet-async";
+
+export default function SEO({
+  title,
+  description,
+  image,
+  url,
+  type = "website",
+  schema = null,
+}) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url} />
+
+      {/* Open Graph */}
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {image && <meta property="og:image" content={image} />}
+      <meta property="og:url" content={url} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
+
+      {/* Optional: schema.org */}
+      {schema && (
+        <script type="application/ld+json">
+          {JSON.stringify(schema)}
+        </script>
+      )}
+    </Helmet>
+  );
+}
