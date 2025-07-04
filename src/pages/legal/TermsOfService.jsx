632d46471f4408d0c6771ee1689a15a047880e5b
AP3HIM <thefartz19@gmail.com>
2025-07-03 20:36:38 -0400

    Update TermsOfService.jsx with SEO

--- src/pages/legal/TermsOfService.jsx
+++ src/pages/legal/TermsOfService.jsx
@@ -1,11 +1,20 @@
 import "../../css/LegalPage.css";
+import SEOStatic from "../../components/SEOStatic";
 
 
 export default function TermsOfService() {
   return (
-    <div className="legal-page">
-      <h1>Terms of Service</h1>
-      <p>By using Paper Tiger Cinema, you agree to access and view only public domain content legally provided through our platform. Misuse, redistribution of content, or unlawful activity may result in account suspension or removal.</p>
-    </div>
+    <>
+      <SEOStatic
+        title="Terms of Service | Paper Tiger Cinema"
+        description="Read the terms and conditions for using Paper Tiger Cinema's website and services."
+        path="/terms"
+      />
+
+      <div className="legal-page">
+        <h1>Terms of Service</h1>
+        <p>By using Paper Tiger Cinema, you agree to access and view only public domain content legally provided through our platform. Misuse, redistribution of content, or unlawful activity may result in account suspension or removal.</p>
+      </div>
+    </>
   );
 }
