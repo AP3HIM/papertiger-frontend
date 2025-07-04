e4c3a6d5d60ff326cdda58f40d7ed5abd827e09e
AP3HIM <thefartz19@gmail.com>
2025-07-03 20:44:25 -0400

    Update SEOHome.jsx with fixed import and export

--- src/components/SEOHome.jsx
+++ src/components/SEOHome.jsx
@@ -1,6 +1,12 @@
-<SEO
-  title="Watch Public Domain Movies Free | Paper Tiger Cinema"
-  description="Explore 800+ classic public domain movies in HD. No ads, no subscriptions."
-  url="https://papertigercinema.com/"
-  image="https://cdn.papertigercinema.com/static/ptc_lgo.png"
-/>
+import SEO from "./SEO";
+
+export default function SEOHome() {
+  return (
+    <SEO
+      title="Watch Public Domain Movies Free | Paper Tiger Cinema"
+      description="Explore 800+ classic public domain movies in HD. No ads, no subscriptions."
+      url="https://papertigercinema.com/"
+      image="https://cdn.papertigercinema.com/static/ptc_lgo.png"
+    />
+  );
+}
