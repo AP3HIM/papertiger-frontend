5963205f7591f53aaf52e457b9cbbeaade1f0d28
AP3HIM <thefartz19@gmail.com>
2025-07-03 20:33:46 -0400

    removing home from url as base url is the home page

--- src/components/SEOHome.jsx
+++ src/components/SEOHome.jsx
@@ -1,6 +1,6 @@
 <SEO
   title="Watch Public Domain Movies Free | Paper Tiger Cinema"
   description="Explore 800+ classic public domain movies in HD. No ads, no subscriptions."
-  url="https://papertigercinema.com/home"
+  url="https://papertigercinema.com/"
   image="https://cdn.papertigercinema.com/static/ptc_lgo.png"
 />
