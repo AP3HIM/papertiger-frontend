307e284e33ca15b1038a564ed3c4ea1ca0e14cc4
AP3HIM <thefartz19@gmail.com>
2025-07-03 20:34:12 -0400

    Update SEOStatic.jsx with description

--- src/components/SEOStatic.jsx
+++ src/components/SEOStatic.jsx
@@ -1,11 +1,14 @@
 import SEO from "./SEO";
 
-export default function SEOStatic({ title, path }) {
+export default function SEOStatic({ title, path, description }) {
   return (
     <SEO
       title={`${title} | Paper Tiger Cinema`}
-      description={`Read our ${title.toLowerCase()} at Paper Tiger Cinema.`}
+      description={
+        description || `Read our ${title.toLowerCase()} at Paper Tiger Cinema.`
+      }
       url={`https://papertigercinema.com${path}`}
     />
   );
 }
+
